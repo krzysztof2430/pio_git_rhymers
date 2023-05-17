714deb7ad74d446fa4681fd0aa212fddaacb6399
krzysztof2323 <krzysztof98k@gmail.com>
2023-05-17 17:11:16 +0200

    13. Utworzenie statycznej metody testRhymers poprzez rozbicie metody main

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -7,6 +7,10 @@ class RhymersDemo {
 
     public static void main(String[] args) {
         RhymersFactory factory = new DefaultRhymersFactory();
+        testRhymers(factory);
+    }
+
+    private static void testRhymers(RhymersFactory factory) {
 
         DefaultCountingOutRhymer[] rhymers = {factory.getStandardRhymer(), factory.getFalseRhymer(),
                 factory.getFIFORhymer(), factory.getHanoiRhymer()};
@@ -25,9 +29,7 @@ class RhymersDemo {
             System.out.println();
         }
 
-        System.out.println("total rejected is "
-                + ((HanoiRhymer) rhymers[3]).reportRejected());
+        System.out.println("total rejected is " + ((HanoiRhymer) rhymers[3]).reportRejected());
 
     }
-
 }
